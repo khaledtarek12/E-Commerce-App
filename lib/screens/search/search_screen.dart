4c99f398d4e6c16caf0ba29e6527fd8f602c3ac9
khaledtarek12 <tareqkhalid092@gmail.com>
2024-09-28 01:30:59 +0300

    finsh ths search field

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,9 +1,32 @@
 import 'package:e_commerce_app/services/assets_manager.dart';
 import 'package:e_commerce_app/widgets/title_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+late TextEditingController controller;
+
+class _SearchScreenState extends State<SearchScreen> {
+  FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +42,38 @@ class SearchScreen extends StatelessWidget {
             ),
           ),
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(8.0),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  enabledBorder: OutlineInputBorder(borderSide: BorderSide()),
-                  focusedBorder: OutlineInputBorder(borderSide: BorderSide()),
+              Focus(
+                onFocusChange: (hasFocus) {
+                  setState(() {}); // Rebuild to update icon color
+                },
+                child: TextField(
+                  controller: controller,
+                  focusNode: focusNode,
+                  decoration: InputDecoration(
+                    filled: true,
+                    prefixIcon: Icon(
+                      IconlyLight.search,
+                      color: focusNode.hasFocus
+                          ? Colors.blue
+                          : Colors.grey, // Change color based on focus
+                    ),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          controller.clear();
+                          FocusScope.of(context).unfocus();
+                        });
+                      },
+                      child: const Icon(
+                        Icons.clear,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
